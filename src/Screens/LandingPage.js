d78eed76f9910c49124fc4a70edecbaa83201ba6
Subhendu-Sipon <subhendusipon2003@gmail.com>
2024-02-03 03:38:16 +0530

    Added Title Card

--- src/Screens/LandingPage.js
+++ src/Screens/LandingPage.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './LandingPage.css'
 import Logo from '../Assets/T Technovation.png'
 import igitLogo from '../Assets/IgitLogo.png'
+import LandingTitle from '../Assets/WhiteFontTitle.jpg'
 
 function LandingPage() {
   return (
     <section className='fullPage'>   
     <img className='logo' src={Logo} alt='logo'></img>
     <img className='igitLogo' src={igitLogo} alt='igitLogo'></img>
+    <div className='LandingTitleT'>
+        <img className='LandingTitle' src={LandingTitle} alt='LandingTitle'></img>
+    </div>
     </section> 
     
   )
